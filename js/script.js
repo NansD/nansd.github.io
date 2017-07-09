c4cde37f26a5614bf2211ec86d993d4d3fa3fb17
NansWattics <nans.dumortier@wattics.com>
2017-07-09 15:38:47 +0100

    add contact form !

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@ $(function(){
     $('html,body').animate({
       scrollTop: $("#about").offset().top
     });
+  });
+  $('.dashboard-link').on("click", function(){
+    $('html,body').animate({
+      scrollTop: $("#dashboard").offset().top
+    });
+  });
+
+  $('.send-mail').on('click', function(){
+    var subject = $('#form32').val();
+    var body = $('#form7').val();
+    window.open('mailto:dumortier.nans@gmail.com?subject=' + subject + '&body=' +body);
   })
 })
