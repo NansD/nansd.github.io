ab1ce13e09cdd4d6700cc1e5ef10e6304113d04b
NansWattics <nans.dumortier@wattics.com>
2017-09-23 19:38:40 +0100

    Work from the 23/09

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(function(){
-  $(".contact").on("click", function(){
-    $('html,body').animate({
-      scrollTop: $(".contact-form").offset().top
-    });
-  });
+  Chart.defaults.global.defaultFontSize = 20
+
+
+   $(".contact").on("click", function(){
+     $('html,body').animate({
+       scrollTop: $(".contact-form").offset().top
+     });
+   });
   $('.dashboard-link').on("click", function(){
     $('html,body').animate({
       scrollTop: $("#dashboard").offset().top
@@ -14,5 +17,21 @@ $(function(){
     var subject = $('#form32').val();
     var body = $('#form7').val();
     window.open('mailto:dumortier.nans@gmail.com?subject=' + subject + '&body=' +body);
-  })
-})
+  });
+
+  var ctx = document.getElementById("myChart");
+  var myRadarChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+      labels: ['Running', 'Swimming', 'Eating', 'Cycling', 'Drinking', 'Sleeping', 'Hiking'],
+      datasets: [{
+          data: [20, 10, 4, 2, 15, 13, 19]
+      }],
+      options: {
+        labels: {
+          fontSize: 20
+        }
+      }
+    }
+  });
+});
